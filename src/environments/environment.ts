bd8a93c28bf9d128c0e3e19526e59d6496a4aa79
Dhruv S Shah <dhruvsshah171@gmail.com>
2026-06-18 15:28:42 +0530

    deployment changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://cafetron-backend-3aof.onrender.com/api',
+  apiUrl: 'https://cafetron-backend-3aof.onrender.com//api',
   wsUrl: 'https://cafetron-backend-3aof.onrender.com/ws',
 };
